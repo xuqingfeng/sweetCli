b7dd0cae2272e43f3431eedb1b14e6a0b25ce876
ftwbzhao <b.zhao1@gmail.com>
2015-07-07 00:24:54 +0800

    change light to bold

--- tests/style.php
+++ tests/style.php
@@ -6,6 +6,6 @@ use Sweet\Cli;
 
 echo Cli::stylize("sweetCli", "black", "white");
 echo Cli::stylize("sweetCli", "underlineMagenta", "cyan");
-echo Cli::stylize("sweetCli", "lightWhite", "yellow");
-
+echo Cli::stylize("sweetCli", "boldWhite", "yellow");
+echo Cli::stylize("sweetCli", "boldRed", "white");
 echo Cli::stylize("sweetCli", 'white');
