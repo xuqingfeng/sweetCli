b6cfba4e7d15001e81660b60c4a6d3a278a02c94
jsxqf <js-xqf@hotmail.com>
2015-07-07 11:44:09 +0800

    update readme

--- readme.md
+++ readme.md
@@ -3,4 +3,8 @@
 
 You may want to check out the [doc](https://xuqingfeng.github.io/doc/sweetCli/)
 
+###Better Solutions
 
+[https://github.com/PeterDaveHello/ColorEchoForShell](https://github.com/PeterDaveHello/ColorEchoForShell)
+
+[http://climate.thephpleague.com/](http://climate.thephpleague.com/)
--- tests/style.php
+++ tests/style.php
@@ -9,3 +9,27 @@ echo Cli::stylize("sweetCli", "underlineMagenta", "cyan");
 echo Cli::stylize("sweetCli", "boldWhite", "yellow");
 echo Cli::stylize("sweetCli", "boldRed", "white");
 echo Cli::stylize("sweetCli", 'white');
+
+//echo Cli::stylize("sweetCli", "black", "white");
+//echo Cli::stylize("sweetCli", "underlineMagenta", "cyan");
+//echo Cli::stylize("sweetCli", "lightWhite", "yellow");
+//
+//echo Cli::stylize("sweetCli", 'white');
+echo PHP_EOL;
+echo PHP_EOL;
+echo PHP_EOL;
+echo Cli::danger('sweetCli').PHP_EOL;
+echo Cli::warning('sweetCli').PHP_EOL;
+echo Cli::primary('sweetCli').PHP_EOL;
+echo Cli::info('sweetCli').PHP_EOL;
+echo Cli::success('sweetCli').PHP_EOL;
+
+echo PHP_EOL;
+echo PHP_EOL;
+echo PHP_EOL;
+
+echo Cli::stylize("sweetCli", "black", "white");
+echo Cli::stylize("sweetCli", "underlineMagenta", "cyan");
+echo Cli::stylize("sweetCli", "boldWhite", "yellow");
+echo Cli::stylize("sweetCli", "boldRed", "white");
+echo Cli::stylize("sweetCli", 'white');
